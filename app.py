249600c743ffc97e6bed43ccd5c5bc68d6f06612
Atharva <atharva0276@gmail.com>
2024-10-13 13:31:41 +0530

    Add app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# import the libraries
+# importing the required modules
 from flask import Flask, render_template, request, jsonify
 import random
 import json
 import pickle
 import numpy as np
 import nltk
-from nltk.stem import WordNetLemmatizer
+from nltk import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
 # download the nltk packages
@@ -16,7 +16,7 @@ nltk.download('wordnet')
 # initialize the flask app
 app = Flask(__name__)
 
-# load the chatbot data and models
+# load the data and model
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
@@ -24,15 +24,16 @@ classes = pickle.load(open('classes.pkl', 'rb'))
 
 chatbot_model = load_model('pikart_customer_service_chatbot.h5')
 
-# function to clean up user sentence
+# function for cleaning the sentence
 def clean_up_sentence(sentence):
     # tokenize
     sentence_words = nltk.word_tokenize(sentence)
 
-    # lemmatize the words
+    # lemmatize
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
 
     return sentence_words
+
 
 # function to create bag of words
 def bag_of_words(sentence):
@@ -40,60 +41,69 @@ def bag_of_words(sentence):
 
     bag = [0]*len(words)
 
-    # create a bag 0f words
+    # create bog
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
-    
+
     return np.array(bag)
 
 
-# function to predict the intent class of the ssentence
+# function for getting class
 def predict_class(sentence):
-    bow = bag_of_words(sentence)  # Create bag-of-words representation
-    res = chatbot_model.predict(np.array([bow]))[0]  # Predict using the model
-    ERROR_THRESHOLD = 0.25  # Set a threshold for confidence
-    # Filter predictions above the threshold
+    bow = bag_of_words(sentence)
+
+    res = chatbot_model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+
+    # filter the redictions using the threshold
+
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    # Sort results by probability in descending order
+
+    # sort the results in descending order
     results.sort(key=lambda x: x[1], reverse=True)
+
     return_list = []
-    # Create a list of intents and probabilities
+
+    # create a list of intents and probabilities
     for r in results:
-        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return_list.append({'intents': classes[r[0]], 'probability': str(r[1])})
+
     return return_list
 
-# function to get a response based on the intent probs
+
+# get responce from chatbot
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
+
     list_of_intents = intents_json['intents']
 
     # search for the matching intent tag
     for i in list_of_intents:
         if i['tag'] == tag:
-            # choose random respose from the available responses
+            # choose random response from set of responses
             result = random.choice(i['responses'])
             break
 
     return result
 
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
-
 # define the route of chatbot
-@app.route("/chat", methods=['POST'])
+@ app.route("/chat", methods=['POST'])
 def chat():
-    # get the user message
+    # get the user sentence
     user_message = request.json.get("message")
 
-    # predict the indent
+    # predict the intent
     ints = predict_class(user_message)
 
-    # get response
+    # get a response
     res = get_response(ints, intents)
 
     # return the response
